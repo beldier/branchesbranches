ed2bdfa1fa106a885e2ea94d40a74aecd7020156
lurianne <luriannemarianne@gmail.com>
2018-10-28 18:33:28 -0400

    operacion

---
+++ src/main/java/Principal.java
@@ -0,0 +1,8 @@
+public class Principal {
+    public static void main(String [] args)
+    {
+        Positivo p = new Positivo();
+        Negativo n = new Negativo();
+        System.out.println(p.sumar(n));
+    }
+}
